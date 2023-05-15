8b4c7c22b41e18045091ec58d5e7a37003da4de3
Mbccode <133646117+Mbccode@users.noreply.github.com>
2023-05-15 10:45:38 +0100

    Add files via upload

---
+++ Exercise1.py
@@ -0,0 +1,9 @@
+#My first python exercise
+
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
